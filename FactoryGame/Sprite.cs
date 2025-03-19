c7dc3519d92c75fa1c4837101062df1a1c2fef8b
Tim Nikitin <tim.nikitin@posteo.de>
2025-03-19 19:07:48 +0100

    Remove Level class; add Sprite class with properties

---
+++ FactoryGame/Sprite.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace FactoryGame
+{
+    abstract class Sprite
+    {
+        private Texture2D _texture;
+        public Vector2 Position;
+        private Vector2 _scale;
+        
+        public Rectangle Rect { 
+            get
+            {
+                return new Rectangle((int)Position.X - _texture.Width / 2, (int)Position.Y - _texture.Height / 2, _texture.Width, _texture.Height);
+            }
+        }
+    }
+}
