00934767f6a77984ed8ec0e9b9c1ede3e3fa1687
gfyAhhDevBoy <tim.nikitin@posteo.de>
2025-03-20 20:49:03 +0100

    Refactor UI elements to use IUIElement interface
    - Updated `TextButtonElement` to implement `IUIElement` instead of inheriting from `UIElement`, adding new properties and methods for rendering and interaction.
    - Removed the `TextElement` and `UIElement` classes along with their associated code.
    - Introduced a new `IUIElement` interface defining `Update` and `Draw` methods for UI elements.

---
+++ FactoryGame/UI/IUIElement.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+
+namespace FactoryGame.UI
+{
+    interface IUIElement
+    {
+        void Update();
+        void Draw(SpriteBatch spriteBatch);
+    }
+}
--- FactoryGame/UI/TextButtonElement.cs
+++ FactoryGame/UI/TextButtonElement.cs
@@ -1,11 +1,42 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace FactoryGame.UI
 {
-    class TextButtonElement : UIElement
+    class TextButtonElement : IUIElement
     {
-        public TextButtonElement(Vector2 position) : base(position)
+        public Vector2 Position;
+        private Rectangle _rect;
+        public string Text;
+        private SpriteFont _font;
+        private bool _highlighted;
+        private Color _color, _highlightColor;
+
+        public TextButtonElement(Vector2 position, string text, SpriteFont font, Color color, Color highlightColor)
         {
+            Position = position;
+            _rect = new((int)position.X - (int)font.MeasureString(text).X, (int)position.Y - (int)font.MeasureString(text).Y, (int) font.MeasureString(text).X, (int) font.MeasureString(text).Y);
+            _color = color;
+            _highlightColor = color;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.DrawString(_font, Text, new(Position.X - _font.MeasureString(Text).X, Position.Y - _font.MeasureString(Text).X), _highlighted ? _highlightColor : _color);
+        }
+
+        public void Update()
+        {
+            Rectangle mouseRect = new(Mouse.GetState().Position, new Point(1, 1));
+            if (mouseRect.Intersects(_rect))
+            {
+                _highlighted = true;
+            } else
+            {
+                _highlighted = false;
+            }
         }
     }
 }
