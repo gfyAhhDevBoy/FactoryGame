ddfe12ab1d78b51b6667b3ec512fd6758148fea1
gfyAhhDevBoy <tim.nikitin@posteo.de>
2025-03-22 22:37:29 +0100

    Refactor Item class to use Player object
    Removed private fields _texture and _scale from the Item class.
    Added a private field _player and updated the constructor to accept a Player object.
    Adjusted the Draw method to utilize the _player field and removed the Rect property.

--- FactoryGame/Items/Item.cs
+++ FactoryGame/Items/Item.cs
@@ -8,26 +8,16 @@ namespace FactoryGame.Items
         int _maxDurability;
         public int Durability;
 
-        private Texture2D _texture;
-        private int _scale;
+        private Player _player;
 
-        public Rectangle Rect
+        public Item(Texture2D texture, Player player, int scale) : base(texture, player.ItemOrigin, new(scale, scale))
         {
-            get
-            {
-                return new(0, 0, _texture.Width * (int)_scale, _texture.Height * (int)_scale);
-            }
+            _player = player;
         }
 
-        public Item(Texture2D texture, int scale)
+        public virtual void Draw(SpriteBatch spriteBatch)
         {
-            _texture = texture;
-            _scale = scale;
-        }
-
-        public virtual void Draw(SpriteBatch spriteBatch, Player player)
-        {
-            Rectangle dest = new((int)(player.ItemOrigin.X - _texture.Width / 2), (int)(player.ItemOrigin.Y - _texture.Height / 2), Rect.Width, Rect.Height);
+            Rectangle dest = new((int)(_player.ItemOrigin.X - _texture.Width / 2), (int)(_player.ItemOrigin.Y - _texture.Height / 2), Rect.Width, Rect.Height);
             spriteBatch.Draw(_texture, dest, Color.White);
         }
     }
